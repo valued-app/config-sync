835dc4f79826e20ac822da9f7b4759ce0ee59de5
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2023-05-09 19:11:47 +0200

    trying out FsLoader in JS action

--- scripts/action.js
+++ scripts/action.js
@@ -1,7 +1,10 @@
 import { setFailed, debug } from "@actions/core";
+import { FsLoader } from "../packages/fs-loader/dist/index.js";
 
 async function run() {
-  debug("hi");
+  const loader = new FsLoader(".");
+  const files = await loader.read(".");
+  debug(JSON.stringify(files, null, 2));
 }
 
 run().catch((error) => {
